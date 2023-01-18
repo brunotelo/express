bb2abc01a885fde56c224e298f9e36be6cd8dff4
Bruno Teló <brunotelo@gmail.com>
2023-01-17 22:57:32 -0300

    Cria Makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+up:
+	docker compose up -d --force-recreate
+bash:
+	docker exec -it python bash   
--- backend/app/database.py
+++ backend/app/database.py
@@ -2,13 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:root@db:3306/db-express"
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:root@db:3306/db-express"
 SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format('root', 'root', 'db', 3306, 'db-express')
-# "mysql+pymysql://sylvain:passwd@localhost/db?host=localhost?port=3306"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL#, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
